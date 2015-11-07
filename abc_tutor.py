29bddd7370067ff2a77702871ce786975d37e16e
Steven Clark <steven.p.clark@gmail.com>
2015-11-07 09:42:28 -0500

    first commit

---
+++ abc_tutor.py
@@ -0,0 +1,62 @@
+import pyglet
+import random
+import colorsys
+
+num_sound_variants = 4
+
+window = pyglet.window.Window(fullscreen=True)
+window.set_exclusive_keyboard()
+
+label = pyglet.text.Label('', font_size=96,
+                            x=window.width//2, y=window.height//2,
+                            anchor_x='center', anchor_y='center')
+
+font_names = ['Geneva', 'Georgia', 'Arial', 'Tahoma', 'Verdana', 'Times New Roman', 'Raleway', 'Courier New', 'Lucida Console']
+
+sound_dict = dict()
+keys = range(97,123) #a-z
+keys.extend(range(48,58)) #0-9
+for k in keys:
+    c = chr(k)
+    variants = []
+    for i in range(num_sound_variants):
+        fn = 'sounds/%s%d.wav'%(c,i)
+        variants.append(pyglet.media.load(fn, streaming=False))
+    sound_dict[c] = variants
+
+a = pyglet.media.load('sounds/a0.wav', streaming=False)
+
+padding = 200
+
+@window.event
+def on_draw():
+    window.clear()
+    label.draw()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    try:
+        c = chr(symbol)
+        variants = sound_dict.get(c, None)
+        if variants:
+            random.choice(variants).play()
+            if random.random() > 0.5:
+                label.text = c
+            else:
+                label.text = c.upper()
+
+            label.x = random.randint(padding, window.width-padding)
+            label.y = random.randint(padding, window.height-padding)
+            label.font_name = random.choice(font_names)
+            label.font_size = random.randint(128,384)
+            #color = [random.randint(100,255) for i in range(3)]
+            color = colorsys.hsv_to_rgb(random.random(), 1, 1)
+            color = [int(x*255) for x in color]
+            color.append(255)
+            label.color = color
+    except ValueError:
+        pass
+
+pyglet.app.run()
+
